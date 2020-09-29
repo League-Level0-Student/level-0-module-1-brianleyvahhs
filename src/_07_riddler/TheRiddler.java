ea0e634d0a1d0c348c59a7635737c2a37043556f
DaveDLeague <dave.dunn@jointheleague.org>
2020-09-29 22:39:12 +0000

    Add files via upload

--- src/_07_riddler/TheRiddler.java
+++ src/_07_riddler/TheRiddler.java
@@ -2,22 +2,26 @@ package _07_riddler;
 //    Copyright (c) The League of Amazing Programmers 2013-2019
 //    Level 0
 
-
+import javax.swing.JOptionPane;
 
 public class TheRiddler {
 
 	public static void main(String[] args) {
 
 		// 1. Make a variable to hold the score
-
+        int Score = 0;
 		// 3. Ask the user a riddle. Here are some ideas: bit.ly/some-riddles
-
-		// 4. If they got the answer right, pop up "correct!" and increase the score by one
-
+        String Answer1 = JOptionPane.showInputDialog("What Can Travel Around The World While Staying In A Corner?");
+        // 4. If they got the answer right, pop up "correct!" and increase the score by one
+        if(Answer1.equals("Stamp")) {
+        	JOptionPane.showMessageDialog("Correct");}
+          }
 		// 5. Otherwise, say "wrong" and tell them the answer
 
 		// 6. Add some more riddles
-
+    	String ??? = JOptionPane.showInputDialog("???");
+    	String ??? = JOptionPane.showInputDialog("???");
+    	String ??? = JOptionPane.showInputDialog("???");
 		// 2. Make a pop up to show the score.
 		
 	}
